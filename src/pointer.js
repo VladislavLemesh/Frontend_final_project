0d58b0591d3558c46a0f24a1a14b31d2f42eb39c
VladislavLemesh <90740323+VladislavLemesh@users.noreply.github.com>
2022-01-27 18:22:27 +1000

    Add files via upload

---
+++ src/pointer.js
@@ -0,0 +1,14 @@
+import './style.css'
+
+let point = document.getElementById("point")
+
+point.addEventListener('mouseover', function( event ) {
+    event.target.style.color = "purple";
+})
+
+point.addEventListener('mouseout', function( event ) {
+    event.target.style.color = "#826C17";
+})
+
+
+
