2d24ce82badd124abde6dfe8cb1578f6d828243e
VladislavLemesh <90740323+VladislavLemesh@users.noreply.github.com>
2022-01-28 20:40:01 +1000

    Add files via upload

---
+++ src/Games/pointer.js
@@ -0,0 +1,9 @@
+let point = document.getElementById("point")
+
+point.addEventListener('mouseover', function( event ) {
+    event.target.style.color = "purple";
+})
+
+point.addEventListener('mouseout', function( event ) {
+    event.target.style.color = "#826C17";
+})
